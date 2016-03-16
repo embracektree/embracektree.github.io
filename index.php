fc6c3c78c8904c7702d8037530eded387cfa2d96
Ktree <ktree.embrace@gmail.com>
2016-03-16 20:43:36 +0530

    faq index3

--- index.php
+++ index.php
@@ -1,16 +1,4 @@
 
 sadfsadf sadfasd sadfsadf asdfsadf asdfsadf
 
-<?php
 
-// comment out the following two lines when deployed to production
-defined('YII_DEBUG') or define('YII_DEBUG', true);
-defined('YII_ENV') or define('YII_ENV', 'dev');
-
-require(__DIR__ . '/vendor/autoload.php');
-require(__DIR__ . '/vendor/yiisoft/yii2/Yii.php');
-require(__DIR__ . '/vendor/KTComponents/KTApplication.php');
-
-$config = require(__DIR__ . '/config/web.php');
-echo "test";die;
-(new app\vendor\KTComponents\KTApplication($config))->run();
